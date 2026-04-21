ca74c5ab4ec33cb5b5ca2812090769caf2f64252
Marcilio Cabral <marcilioccsilva@fac.pe.senac.br>
2026-04-21 03:26:02 -0300

    Bug carregamento

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,11 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Server
   }
 ];
