d017f7593f1e3da3c9463e16687c10c55c0094da
Marcilio Cabral <marcilioccsilva@fac.pe.senac.br>
2026-05-04 13:09:03 -0300

    Transformando em app desktop (WIP)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+// import { routes } from './app.routes';
+// import { provideHttpClient, withFetch } from '@angular/common/http';
+// import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(withFetch())
+    // provideBrowserGlobalErrorListeners(),
+    provideRouter(routes, withHashLocation())
+    // provideHttpClient(withFetch())
   ]
 };
